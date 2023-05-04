935ca4476e3776567e2c2d787e22a97bab765487
Sunita Dhruw <116973136+dhruwsunita@users.noreply.github.com>
2023-05-04 17:00:26 +0530

    initial commit

---
+++ Group_Assignment2/src/skyscraper/Driver.java
@@ -0,0 +1,18 @@
+package skyscraper;
+
+import skyscraper.SkyscraperBuilder;
+
+public class Driver {
+
+	public Driver() {
+		// TODO Auto-generated constructor stub
+	}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		SkyscraperBuilder builder=new SkyscraperBuilder();
+		builder.manager();
+
+	}
+
+}
